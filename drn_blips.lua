dade7899c2bd2bc724c196e63e79d6f1e4ce72fa
itzdroyon <73495823+itzdroyon@users.noreply.github.com>
2021-09-03 05:16:21 +0200

    Update drn_blips.lua

--- drn_blips.lua
+++ drn_blips.lua
@@ -1,7 +1,7 @@
 
 local blips = {
 -- Ej: {title="", colour=, id=, x=, y=, z=},
-     {title="Bahama Mamas Club", colour=3, id=93, x = -1388.16, y = -591.28, z = 30.32},
+     {title="Bahama Mamas Club", colour=3, id=93, size=1.0, x = -1388.16, y = -591.28, z = 30.32},
    --{title="Marlowe Vineyard", colour=3, id=499, x = -1873.58, y = 2023.76, z = 138.83}
   }
       
@@ -11,7 +11,7 @@ Citizen.CreateThread(function()
       info.blip = AddBlipForCoord(info.x, info.y, info.z)
       SetBlipSprite(info.blip, info.id)
       SetBlipDisplay(info.blip, 4)
-      SetBlipScale(info.blip, 1.0)
+      SetBlipScale(info.blip, info.size)
       SetBlipColour(info.blip, info.colour)
       SetBlipAsShortRange(info.blip, true)
 	  BeginTextCommandSetBlipName("STRING")
